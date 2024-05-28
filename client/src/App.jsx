81b38c04bbd8df8f2562cba529ddba63a08c815e
deepjoy9 <deepjoysarkar9@gmail.com>
2024-05-28 20:58:07 +0530

    Set up React app with Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div>My Personal Projects</div>
+      <h1 className="text-3xl font-bold">My Personal Projects</h1>
     </>
   );
 }
