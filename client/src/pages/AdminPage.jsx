04d3c16bb6734dc824fea3a6b835a46b9b859c88
deepjoy9 <deepjoysarkar9@gmail.com>
2024-05-31 01:21:36 +0530

    feat: Add AdminPage component for project management

---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import AddCard from "../components/AddCard";
+
+const AdminPage = () => {
+  return (
+    <div className="admin-page">
+      <h1>Admin Page : </h1>
+      <AddCard />
+    </div>
+  );
+};
+
+export default AdminPage;
