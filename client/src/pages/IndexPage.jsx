e48ae284cc3302c662597e72453c96fe24880d23
deepjoy9 <deepjoysarkar9@gmail.com>
2024-06-01 23:41:56 +0530

    feat: Add IndexPage component for rendering main content

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import CardList from "../components/CardList";
+
+const IndexPage = () => {
+  return (
+    <div className="index-page">
+      <CardList />
+    </div>
+  );
+};
+
+export default IndexPage;
