37737db2abd54e4c59ab8b9bf5d8ca1b5b851f1f
deepjoy9 <deepjoysarkar9@gmail.com>
2024-06-13 21:06:56 +0530

    Add login page, user context, and toast notifications
    - Created Login component for user authentication.
    - Implemented UserContext to store and manage user information globally.
    - Integrated react-toastify for toast notifications.
    - Added login functionality to authenticate and redirect to the admin page.
    - Enhanced user experience with toast notifications for login success and errors.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
 import "./App.css";
-import CardList from "./components/CardList";
 import { Route, Routes } from "react-router-dom";
 import AddCard from "./components/AddCard";
 import Layout from "./components/Layout";
 import AdminPage from "./pages/AdminPage";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import { UserContextProvider } from "./contexts/UserContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="admin" element={<Layout />}>
-          <Route index element={<AdminPage />} />
-          <Route path="add" element={<AddCard />} />
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="admin" element={<Layout />}>
+            <Route index element={<AdminPage />} />
+            <Route path="add" element={<AddCard />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,71 @@
+import React, { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { toast } from "react-toastify";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+
+  async function login(ev) {
+    ev.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const userInfo = await response.json();
+        setUserInfo(userInfo);
+        toast.success("You have successfully logged in.");
+        setRedirect(true);
+      } else {
+        const errorMessage = await response.json();
+        throw new Error(
+          errorMessage.error || "Failed to log in. Please try again later."
+        );
+      }
+    } catch (error) {
+      console.error("Error logging in:", error.message);
+      toast.error(
+        error.message || "An unexpected error occurred. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/admin"} />;
+  }
+
+  return (
+    <>
+      <form className="login" onSubmit={login}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button>{loading ? "Loading..." : "Login"}</button>
+      </form>
+    </>
+  );
+};
+
+export default LoginPage;
