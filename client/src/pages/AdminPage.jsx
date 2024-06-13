87fdb65919de1f52e26717e1d0b522522bf53ccc
deepjoy9 <deepjoysarkar9@gmail.com>
2024-06-13 21:08:28 +0530

    Add user authentication to AdminPage with navigation guard

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,7 +1,19 @@
 import CardList from "../components/CardList";
 import AddProjectCard from "../components/AddProjectCard";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { useContext, useEffect } from "react";
 
 const AdminPage = () => {
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    }
+  }, [navigate, userInfo]);
+
   return (
     <div className="admin-page">
       <h1>Admin Page</h1>
