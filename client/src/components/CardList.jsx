7b7f02e753d577ee97b416ef03d93779e0cfb14c
deepjoy9 <deepjoysarkar9@gmail.com>
2024-06-19 21:17:22 +0530

    Refactor CardList to use local JSON file for projects data

--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -1,33 +1,35 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import { BASE_URL } from "../utils/apiConstants";
+import projects from "../data/projects.json";
+
 
 const CardList = () => {
-  const [projects, setProjects] = useState([]);
-  const [error, setError] = useState(null);
+  // const [projects, setProjects] = useState([]);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch(`${BASE_URL}/projects`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((projects) => {
-        setProjects(projects);
-      })
-      .catch((error) => {
-        setError(error);
-        console.error("Error fetching projects:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${BASE_URL}/projects`)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((projects) => {
+  //       setProjects(projects);
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //       console.error("Error fetching projects:", error);
+  //     });
+  // }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
 
-  console.log("Projects:", projects);
+  // console.log("Projects:", projects);
 
   return (
     <div className="cards-wrapper">
