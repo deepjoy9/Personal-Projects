80905798cd313c9e68e2171cb262a363bdeacd6f
deepjoy9 <deepjoysarkar9@gmail.com>
2024-06-25 23:15:21 +0530

    Add Contact page with CSS styling

---
+++ client/src/pages/Contact/ContactPage.jsx
@@ -0,0 +1,34 @@
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils/utils";
+
+const ContactPage = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">deepjoysarkar9@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/deepjoy9">
+            linkedin.com/deepjoy9
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/deepjoy9">github.com/deepjoy9</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default ContactPage;
